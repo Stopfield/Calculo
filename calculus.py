de4017497275fba996aaf8f69e5acb1cef6493ef
Stopfield <thiagoparacampo@hotmail.com>
2019-11-10 21:01:50 -0300

    Adicionando menu no inicio do programa

--- calculus.py
+++ calculus.py
@@ -1,1 +1,5 @@
 # Função que a gente faz as resoluções dos projetos
+# import numpy as np;
+
+def printaAlgo():
+    print('Alguma coisa bem totoooooop');
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 # Função em que a gente resolve os problemas
-from calculus import *;
+from calculus import * # Importa as funções e métodos do "calculos.py"
 
+while True:
+    choice = int(input('Escolha a questão a ser resolvida: '));
 
+    # Sair do programa
+    if (choice <= 0):
+        break;
+    # Projeto 1
+    elif (choice == 1):
+        pass; # projeto1();
+    # Projeto 2
+    elif (choice == 2):
+        pass; # projeto2();
+    else:
+        print('Opção inválida!\n');
