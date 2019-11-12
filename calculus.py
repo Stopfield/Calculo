2c479731adaad80a4092a83652d659d79e1ec468
Stopfield <thiagoparacampo@hotmail.com>
2019-11-12 10:04:13 -0300

    Adicionado o arquivo calculus.py

---
+++ calculus.py
@@ -0,0 +1,12 @@
+# Aqui é onde os projetos serão resolvidos
+from projectDefinition import *
+
+# Para a projeto 2
+
+
+projeto2 = Projeto2(0, 2);
+
+
+# Para o projeto 5
+
+projeto5 = Projeto5(0, 2);
