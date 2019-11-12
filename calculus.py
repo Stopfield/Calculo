0b26463d630d0d7dbaded67fff22bac09666987b
Stopfield <thiagoparacampo@hotmail.com>
2019-11-12 08:56:38 -0300

    Adicionando a funçao ESQ() para um projeto so

--- calculus.py
+++ calculus.py
@@ -5,6 +5,21 @@ import math as mt;
 
 
 
-# Função para a qual faremos os cálculos e acharemos as áreas
-def funcao(x = 0):
-    pass;
+# Funções para a qual faremos os cálculos e acharemos as áreas
+def funcaoProjeto2(x = 0):
+    return mt.e ** (x - 1); # Do intervalo de 0 a 2
+
+def funcaoProjeto5(x = 0):
+    return -(x ** 2) + 4;   # Do intervalo de 0 a 2
+
+# Função que calcula a área a partir do ponto esquerdo
+def ESQ(intervalo1, intervalo2, n = 0):
+    base = (intervalo2 - intervalo1) / n;
+    x = intervalo1;
+    area = 0;
+
+    for i in range(n):
+        # area = y * x 
+        area = funcaoProjeto2(x) * base;
+        x += base;
+    return area;
