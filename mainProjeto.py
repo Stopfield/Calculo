fe5936c5c6e57ac7835c7db6b13772b9a969aadf
Stopfield <thiagoparacampo@hotmail.com>
2019-11-12 09:30:33 -0300

    Adicionando funçao que calcula a area a partir do ponto medio

--- mainProjeto.py
+++ mainProjeto.py
@@ -35,8 +35,18 @@ class Projeto:
             x += base;
         return area;
     
-    # Função que calcula a área a partir de trapézios ao invés de retângulos
-    def TRAP(self, intervalo1, intervalo2, n = 0):
-        pass;
+    # Função que calcula a área a partir do ponto médio
+    def MED(self, intervalo1, intervalo2, n = 0):
+        base = (intervalo2 - intervalo1) / n;
+        x = intervalo1;
+        area = 0;
+        pontoMedio = (x + base) / 2;
+
+        for i in range(n):
+            # Aqui pega-se o valor da função no ponto médio
+            # area = y * x 
+            area = self.funcaoProjeto(pontoMedio) * base;
+            x += base;
+        return area;
 
     
